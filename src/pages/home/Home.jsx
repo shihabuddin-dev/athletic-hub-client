61c01789c21ef38179787c4325a60f4a5a05decc
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-08-09 11:24:18 +0600

    react fast marquee inplemented here

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,12 +4,14 @@ import Testimonials from "../../components/homeComponents/Testimonials";
 import PopularSports from "../../components/homeComponents/PopularSports";
 import AthleticStats from "../../components/homeComponents/AthleticStats";
 import HowItWorks from "../static/HowItWorks";
+import Sports from "../../components/homeComponents/Sports";
 
 const Home = () => {
   return (
     <div className="space-y-16 md:space-y-24">
       <HeroSlider />
       <FeaturesEvents />
+      <Sports/>
       <PopularSports />
       <AthleticStats />
       <HowItWorks/>
